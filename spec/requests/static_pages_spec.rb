f35b8aa69272bbd913f2773983cfeb1b68509442
ingmar <i_ngmar@hotmail.com>
2014-01-16 10:13:44 +0100

    Add an bunch of tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,41 +2,34 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  subject { page }
+
   describe "Home page" do
-
-    it "should have the content 'Sample App' " do
-
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-
-    end
-
-    it "should have not a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => "| Home")
-    end
-
+    before { visit root_path }
+    it { should have_selector('h1', :text => 'First App') }
+    it { should_not have_selector('title', :text => full_title(" ") ) }
   end
 
   describe "Help page" do
-
-    it "should have the content 'Help' " do
-
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-
-    end
-
+    before { visit help_path }
+    it { should have_selector('h1', :text => 'Help') }
+    it { should have_selector('title', :text => full_title("Help")) }
   end
 
   describe "About page" do
+    before { visit about_path }
+    it { should have_selector('h1', :text => 'About us') }
+    it { should have_selector('title', :text => full_title("About us")) }
+  end
 
-    it "should have the content 'About us' " do
-
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About us')
-
-    end
-
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', :text => full_title("About us")
+    click_link "Help"
+    page.should have_selector 'title', :text => full_title("Help")
+    click_link "Home"
+    click_link "Sign up"
+    page.should have_selector 'title', :text => full_title("Sign up")
   end
 end
